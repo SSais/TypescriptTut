9246c66931f48bbb39ff02ce7c1a1c3a90c6e0c8
Sayed <sayed.sais@gmail.com>
2024-09-13 09:20:26 +0100

    added exercise 1

---
+++ Exercise1.js
@@ -0,0 +1,13 @@
+//For this exercise determine the types of the following data:
+
+let string = 'hello';
+
+let number = 2;
+
+let unkown = undefined; 
+
+let object = { day: 1}
+
+let array = [1, 2]
+
+// Did you notice something strange, why is that the case?
